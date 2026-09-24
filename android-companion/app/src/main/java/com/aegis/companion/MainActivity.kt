a8646445f8c446c5c157b77bae804b2c4740b5e9
SUDHANSHU KUMAR <kumarsudanshu45@gmail.com>
2026-09-24 16:09:36 +0530

    Add trusted runner and Android companion foundation

---
+++ android-companion/app/src/main/java/com/aegis/companion/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.aegis.companion
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState); startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)) }
+}
